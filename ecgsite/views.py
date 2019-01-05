a26200fb05e33d073de8170085b5f393e9c601f5
AnnaJanus <anusiula.789@gmail.com>
2019-01-05 02:01:29 +0100

    start

--- ecgsite/views.py
+++ ecgsite/views.py
@@ -7,5 +7,4 @@ from .models import File
 # Create your views here.
 def ecg_list(request):
     File.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
-    return render(request, 'ecgsite/chart.html', {})
-  return render(request, 'ecgsite/ecg_list.html', {})
+    return render(request, 'ecgsite/ecg_list.html', {})
